e1a41f4db8453f7d18864091b5a35f3bc98996c9
Vika <vikawhynot@yandex.ru>
2026-05-20 23:07:56 +0300

    update Lesson_3

---
+++ Lesson_3/app.js
@@ -0,0 +1,6 @@
+const titleProject = "Оценка стоимости";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 1500; 
+const percentage = 15;    
+const fullPrice = 120000; 
+const responsive = true; 
